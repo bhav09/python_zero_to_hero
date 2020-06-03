90c3a6e6d7ba5442201189f3c895a4427b35be70
Bhavishya Pandit <50860603+bhav09@users.noreply.github.com>
2020-06-03 20:55:06 +0530

    Add files via upload

---
+++ conditional_statements/ifelsepgm.py
@@ -0,0 +1,13 @@
+
+cpu=100
+qty=int(input('Enter quantity'))
+cost=qty*100
+if cost>1000:
+    cost-=cost/10
+
+#elif condition:
+    #result
+
+#else:
+    #result
+print(cost)
---
+++ conditional_statements/switchcase.py
@@ -0,0 +1,19 @@
+#Using switch case in python
+
+print('1. Addition')
+print('2. Subtraction')
+print('3. Multiplication')
+print('4. Division')
+a=int(input())
+b=int(input())
+choice=int(input('Enter Choice:'))
+operation={
+        1:a+b,
+        2:a-b,
+        3:a*b,
+        4:a/b
+    }
+if choice in operation:
+    print(operation[choice])
+else:
+    print('Wrong choice')
